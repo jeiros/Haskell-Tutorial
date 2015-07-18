392fbda30ebdd7bdd2abff53ea75a50d40fc3f25
Juan Eiros <j.eiros-zamora14@imperial.ac.uk>
2015-07-18 18:37:30 +0100

    Partial functions

---
+++ functions_page5.hs
@@ -0,0 +1,14 @@
+-- Functions from http://learnyouahaskell.com/higher-order-functions
+
+
+
+multThree :: (Num a) => a -> a -> a -> a  
+multThree x y z = x * y * z 
+
+
+multTwoWithNine :: (Num a) => a -> a -> a
+multTwoWithNine = multThree 9
+
+multWithEighteen :: (Num a) => a -> a
+multWithEighteen = multTwoWithNine 2
+
